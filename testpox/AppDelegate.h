fb54e0d2915a5b8632ba6330988910407e014b81
Learning2012 <398188662@qq.com>
2014-08-15 22:57:54 +0800

    proxy

---
+++ testpox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testpox
+//
+//  Created by kacper.wang on 14-8-15.
+//  Copyright (c) 2014年 kacper.wang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
